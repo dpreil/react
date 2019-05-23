ec03cdc4833fa6acd84a70e4df507710f18ac8f9
DP <dzpreil@gmail.com>
2019-05-24 00:01:59 +0300

    clarifai api included
    Box set. Reset working.

--- smartbrain/src/imageReader.js
+++ smartbrain/src/imageReader.js
@@ -8,6 +8,8 @@ import TiltBox from './tiltbox';
 import InputField from './inputField';
 import ImageBox from './imagebox';
 import Logout from './logout';
+import Clarifai from 'clarifai';
+
 
 
 class ImageReader extends Component {
@@ -15,18 +17,31 @@ class ImageReader extends Component {
     super(props);
     this.state = {
       imgURL: '',
+      box:''
     }
-
+    this.app = new Clarifai.App({apiKey:'37ee56e7f2144e0d873763d314893fa5'});
   }
+  
+
 
   getImg = (event) => {
     event.preventDefault();
-    console.log(event.target.value);
-    this.setState({imgURL:event.target.value});
+    this.setState({imgURL:event.target.value, box:''});
+    console.log(this.state.box);
   }
 
   submitImage = (e) => {
     e.preventDefault();
+    this.app.models.predict("a403429f2ddf4b49b307e318f00e528b",this.state.imgURL)
+    .then((response) => {
+      const path = response.outputs[0].data.regions[0].region_info.bounding_box;
+     // console.log(path);
+      this.setState({box:path});
+
+    }, (err) => {
+
+    })
+
     //submit image for rendering and processing
     // I know enough to render in new div. need to fix use Input Field 
   }
@@ -38,7 +53,7 @@ class ImageReader extends Component {
         <TiltBox/>
  
         <InputField getIMG={this.getImg} submitImage={this.submitImage}/>
-        <ImageBox ImgUrl = {this.state.imgURL}/>
+        <ImageBox ImgUrl = {this.state.imgURL} box={this.state.box}/>
         <Logout logout={this.props.logout}/>
       </div>
     );
--- smartbrain/src/imagebox.js
+++ smartbrain/src/imagebox.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
-const ImageBox = ({ImgUrl}) => {
-		if(ImgUrl!==''){
+class ImageBox extends React.Component {
+	constructor(props){
+		super(props);
+		this.canvasRef = React.createRef();
+	}
+
+	componentDidUpdate() {
+		if(this.props.ImgUrl){
+			const canvas = this.canvasRef.current;
+			const ctx = canvas.getContext('2d');
+			const width = canvas.width;
+			const height = canvas.height;
+			if(!this.props.box){
+				canvas.width = canvas.width;
+			} else {
+				const corners = this.props.box;
+				const xStart = corners.left_col*canvas.width;
+				const yStart = corners.top_row*canvas.height;
+				const boxWidth = (corners.right_col-corners.left_col)*canvas.width;
+				const boxHeight = (corners.bottom_row-corners.top_row)*canvas.height;
+				ctx.rect(xStart,yStart,boxWidth,boxHeight);
+				ctx.strokeStyle="red";
+				ctx.stroke();
+			}
+		}
+	}
+
+	render(){
+		const img=this.props.ImgUrl;
+		if(img!==''){
 				return(
 					<div className="imageContainer">
-						<img className='imagebox pa1' src={ImgUrl} alt='Please enter a valid URL.'/>
+						<canvas ref = {this.canvasRef} className="canvas"/ >
+						<img className='imagebox pa1' src={img} alt='Please enter a valid URL.'/>
 					</div>
 					)
 			} else {
-				return (<div/>)
+				return (<div className="empty"/>)
 			}
+	}
 }
 
 export default ImageBox;
